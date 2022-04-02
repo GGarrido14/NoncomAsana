e35d98440e05f3acfdcec3c66d8a9dfd69ad8d81
Guillermo Garrido Jiménez <ggarrido@atalayagroups.com>
2022-04-02 19:03:35 +0200

    Cambios en el modelo de datos

--- asana/models.py
+++ asana/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 
-class Sprint(models.Model):
-    sprint_name = models.CharField(max_length=999)
-
-    
-class Project(models.Model):
-    project_name = models.CharField(max_length=999)
-    
-class Assignee(models.Model):
-    person_name = models.CharField(max_length=999)
+#--------------------------------------------------- class Sprint(models.Model):
+    #---------------------------- sprint_name = models.CharField(max_length=999)
+#------------------------------------------------------------------------------ 
+#------------------------------------------------------------------------------ 
+#-------------------------------------------------- class Project(models.Model):
+    #--------------------------- project_name = models.CharField(max_length=999)
+#------------------------------------------------------------------------------ 
+#------------------------------------------------- class Assignee(models.Model):
+    #---------------------------- person_name = models.CharField(max_length=999)
     
 class Task(models.Model):
     PRIORITIES_COMPLEXITIES = [
@@ -23,16 +23,18 @@ class Task(models.Model):
     ('C', 'Completed'),
     ('A', 'approved')
     ]
-    task_name = models.CharField(primary_key=True, max_length=999)
-    task_notes = models.CharField(max_length=999)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    worked_hours = models.IntegerField()
-    estimated_hours = models.IntegerField()
-    priority = models.CharField(max_length=1, choices=PRIORITIES_COMPLEXITIES)
-    complexity = models.CharField(max_length=1, choices=PRIORITIES_COMPLEXITIES)
-    assignee = models.ForeignKey(Assignee, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)    
-    sprint = models.ForeignKey(Sprint, on_delete=models.CASCADE)
+    id = models.IntegerField(primary_key=True, default=None)
+    name = models.CharField(max_length=999, default='')
+    notes = models.CharField(max_length=999)
+    assignee = models.CharField(max_length=999)
+    project = models.CharField(max_length=999)  
+    sprint = models.CharField(max_length=999)    
+    start_date = models.DateField(null=True, default = None)
+    end_date = models.DateField(null=True, default = None)
+    worked_hours = models.IntegerField(null=True, default = None)
+    estimated_hours = models.IntegerField(null=True, default = None)
+    priority = models.CharField(max_length=1, choices=PRIORITIES_COMPLEXITIES, null=True, default = None)
+    complexity = models.CharField(max_length=1, choices=PRIORITIES_COMPLEXITIES, null=True, default = None)
+
     
 
--- asana/views.py
+++ asana/views.py
@@ -43,62 +43,55 @@ teams_gid = jsonpath_expr_gid.find(team_projects_json_data)
 
 # Get teams project
 for team_gid in teams_gid:
-    #   print(f'Get multiple tasks by project teams gid: {teams_gid.value}')  
+  
     for projects_gid in {team_gid.value}:
         multiple_tasks_endpoint = endpoint + '/tasks?project=' + projects_gid
         multiple_tasks_json = requests.get(multiple_tasks_endpoint, headers=headers).json()
         multiple_tasks_json_data = json.loads(json.dumps(multiple_tasks_json))           
-        #print(f'Get a tasks by project gid: ', jsonpath_expr_gid.find(multiple_tasks_json_data))        
+       
         for task in jsonpath_expr_gid.find(multiple_tasks_json_data):               
             for task_gid in {task.value}: 
-                task = models.Task()    
+                task = models.Task()
+                task.id = task_gid
                 get_task_endpoint = endpoint + '/tasks/' + task_gid
                 get_task_json = requests.get(get_task_endpoint, headers=headers).json()
-                get_task_json_data = json.loads(json.dumps(get_task_json))                                
-                #print("task_json_data ", get_task_json_data)
+                get_task_json_data = json.loads(json.dumps(get_task_json))    
                 jsonpath_expr_a_name= parse('$.name')
 
                 jsonpath_expr_project = parse('data[*].projects')
                 task_project = jsonpath_expr_project.find(get_task_json_data)                
                 task_project_data = task_project[0].value
-                #print("Proyecto data", task_project_data)
+                
                 for data in task_project_data:
                     project_json_data = json.loads(json.dumps(data))
-                    #print("Json del proyecto ", project_json_data)
-                    project_name = jsonpath_expr_a_name.find(project_json_data)                   
-                    #print("Nombre del proyecto ", project_name[0].value)
+                    project_name = jsonpath_expr_a_name.find(project_json_data)     
+                    task.project = project_name
                     
                 #Busca en el json el grupo que representa a la persona asignada
                 jsonpath_expr_assignee = parse('data[*].assignee')                
-                task_assignee  = jsonpath_expr_assignee.find(get_task_json_data)
-                #print("Persona asignada ", task_assignee)
+                task_assignee  = jsonpath_expr_assignee.find(get_task_json_data)               
                 task_assignee_data = task_assignee[0].value
+                
                 #Si la persona asignada existe para la tarea
-                if (task_assignee_data is not None):
-                    
-                    #print("Persona asignada data ", task_assignee_data)
-                    
+                if (task_assignee_data is not None):              
+                                                         
                     #Convierte el dato encontrado en json para que entonces
                     assignee_json_data = json.loads(json.dumps(task_assignee_data))
-                    
-                    #print("Persona asignada json data ", assignee_json_data)                    
+                           
                     #Encontrarlo con el patron definido y definir una variable que almacene el nombre
-                    assignee_name = jsonpath_expr_a_name.find(assignee_json_data)                    
-                    #print("Nombre persona asignada ", assignee_name[0].value)
-                    #task.assignee.person_name = assignee_name[0].value
+                    assignee_name = jsonpath_expr_a_name.find(assignee_json_data)       
+                    task.assignee = assignee_name[0].value
                     
                     jsonpath_expr_name = parse('data[*].name')
-                    task_name  = jsonpath_expr_name.find(get_task_json_data)                    
-                    print('Nombre tarea: ', task_name[0].value)
-                    task.task_name = task_name[0].value
+                    task_name  = jsonpath_expr_name.find(get_task_json_data) 
+                    task.name = task_name[0].value
 
                     jsonpath_expr_note = parse('data[*].notes')
-                    task_note  = jsonpath_expr_note.find(get_task_json_data)                    
-                    print('Descripcion tarea: ', task_note[0].value)
-                    task.task_note = task_note[0].value
-                                        
-
+                    task_note  = jsonpath_expr_note.find(get_task_json_data)            
+                    task.notes = task_note[0].value   
                 
+                task.save()
+                                                                  
 
 
 
